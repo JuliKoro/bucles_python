88a8b971ba0d3826de92d70617058914166a71cf
Julián Koroluk <julian.koroluk@outlook.com>
2021-05-31 19:12:57 -0300

    Hecho profundicacion_2.py
    Resuelto el ejercicio 2 de profundización

--- ejercicios_profunidzacion/profundizacion_2.py
+++ ejercicios_profunidzacion/profundizacion_2.py
@@ -35,3 +35,28 @@ alguno de lo soportados o no es la palabra "FIN".
 
 print("Mi Calculadora (^_^)")
 # Empezar aquí la resolución del ejercicio
+menu = 'OK' # Variable que controla el while seteada para que entre
+while menu != 'FIN':
+    menu = input('\nEliga la operación que desee realizar (ingrese el símbolo correspondiente):\n'
+                '- Suma (+)\n- Resta (-)\n- Multiplicación (*)\n- División (/)\n- Exponente/Potencia (**)'
+                '\n Ingrese "FIN" para cerrar la calculadora.\n')
+    if menu == 'FIN' or menu == 'fin' or menu == 'Fin': # Cuando el usuario quiera terminar
+        break # Sale del while y el programa termina
+    elif menu == '+' or menu == '-' or menu == '*' or menu == '/' or menu == '**': # En caso de que haya ingresado una operación
+        print('A continuación ingrese 2 números para realizar una operación.') # Pedido de nros.
+        num_1 = int(input('Ingrese un número para operar: '))
+        num_2 = int(input('Ingrese otro número para operar: '))
+        if menu == '+': # Suma
+            resultado = num_1 + num_2
+        elif menu == '-': # Resta
+            resultado = num_1 - num_2
+        elif menu == '*': # Multiplicación
+            resultado = num_1 * num_1
+        elif menu == '/': # División
+            resultado = num_1 / num_2
+        elif menu == '**': # Exponente/Potencia
+            resultado = num_1 ** num_2
+        print(f'El resultado de operar {num_1} {menu} {num_2} es: {resultado}') # Se imprime el resultado
+    else: # En caso de que ingrese un caracter invalido a la variable 'menu'
+        print(f'Error, {menu} no es un carácter válido. Por favor ingrese nuevamente.')
+        
